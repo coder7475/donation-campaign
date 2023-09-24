764df0e35bc292fa6ee96c86c9560228dcf89d15
coder7475 <robiulhossain7475@gmail.com>
2023-09-24 20:40:59 +0600

    feat: add three menu items name with their typography in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div className="mt-12">
+        <div className="mt-12 flex justify-between items-center font-inter">
             <figure>
                 <img src="/Resources/Logo.png" alt="logo" />
             </figure>
+            <nav className="flex flex-col md:flex-row gap-12 text-[#0B0B0B] text-lg font-normal">
+                <NavLink>Home</NavLink>
+                <NavLink>Donation</NavLink>
+                <NavLink>Statistics</NavLink>
+            </nav>
         </div>
     );
 };
