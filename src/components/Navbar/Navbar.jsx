47599083062b1823294bb5c7de4a064f8341a85c
coder7475 <robiulhossain7475@gmail.com>
2023-09-24 20:46:56 +0600

    feat: make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="mt-12 flex justify-between items-center font-inter">
-            <figure>
+        <div className="mt-12 flex justify-center lg:justify-between items-center font-inter flex-col lg:flex-row gap-5 mx-2">
+            <figure className="text-center">
                 <img src="/Resources/Logo.png" alt="logo" />
             </figure>
-            <nav className="flex flex-col md:flex-row gap-12 text-[#0B0B0B] text-lg font-normal">
+            <nav className="flex flex-col justify-center md:flex-row gap-4 md:gap-12 text-[#0B0B0B] text-lg font-normal">
                 <NavLink>Home</NavLink>
                 <NavLink>Donation</NavLink>
                 <NavLink>Statistics</NavLink>
