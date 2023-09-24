43706fab184ba2c1cd7793cebc3ba1991a7df79b
coder7475 <robiulhossain7475@gmail.com>
2023-09-24 22:39:33 +0600

    fix: Back To HomePage button

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -12,9 +12,12 @@ const ErrorPage = () => {
                                 doorway to the great nothing
                             </h1>
                             <p className="my-2 text-gray-800">Sorry about that! Please visit our homepage to get where you need to go.</p>
-                            <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                            <Link to={`/`}>Back To HomePage</Link>
-                            </button>
+                            <Link to={`/`}>
+                                <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                                    Back To HomePage
+                                </button>
+                                
+                            </Link>
                         </div>
                     </div>
                     <div>
