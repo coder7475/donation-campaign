a004c17f1bd93737df24afd12b02c5bbf2f94f76
coder7475 <robiulhossain7475@gmail.com>
2023-09-24 21:39:59 +0600

    feat: add background image overlay

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -3,8 +3,14 @@ import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-[1320px] mx-auto">
-            <Navbar></Navbar>
+        <div className="">
+            <div className="relative">
+                <img src="https://images.unsplash.com/photo-1593113630400-ea4288922497?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="donation+image" className="absolute inset-0 h-full w-full object-cover" />
+                <div className="absolute inset-0 bg-neutral-200 bg-opacity-80"></div>
+                <div className="relative max-w-[1320px] mx-auto pt-12">
+                    <Navbar></Navbar>
+                </div>
+            </div>
             <Outlet></Outlet>      
         </div>
     );
