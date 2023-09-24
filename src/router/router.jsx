639d3042c48be597d3bfb38dc93ac921b7066ce4
coder7475 <robiulhossain7475@gmail.com>
2023-09-24 22:37:30 +0600

    feat: add a error page for error element in main route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,11 +3,13 @@ import MainLayout from "../MainLayout/MainLayout"
 import Home from "../pages/Home/Home";
 import Donation from "../pages/Donation/Donation";
 import Statistics from "../pages/Statistics/Statistics";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const myRouter = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/home',
