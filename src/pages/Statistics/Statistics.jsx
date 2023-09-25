722353d851b1484d4382329867f072ab34a09daf
coder7475 <robiulhossain7475@gmail.com>
2023-09-25 20:22:50 +0600

    feat: add a pie chart for statistics page as per figma

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,10 +1,61 @@
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+
+
 const Statistics = () => {
+    const donations = localStorage.getItem('donations');
+    const parsed = JSON.parse(donations);
+    const your = parsed.length;
+    const total = 12 - your;
+    // console.log(parsed.length);
+
+    const data = [
+        { name: 'Total Donation', value: total }, // Replace with your actual total donation amount
+        { name: 'Your Donation', value: your },   // Replace with your actual donation amount
+    ];
+    const RADIAN = Math.PI / 180;
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+      
+        return (
+          <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central" className='font-bold text-3xl font-inter'>
+            {`${(percent * 100).toFixed(1)}%`}
+          </text>
+        );
+      };
     return (
-        <div>
-            <h1 className="text-5xl text-center mt-10">
-                Statistics Page
-            </h1>            
-        </div>
+        <div className='flex justify-center mb-44 mt-24'>
+            <PieChart width={600} height={600}>
+                <Pie
+                    data={data}
+                    startAngle={360}
+                    endAngle={0}
+                    paddingAngle={2}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={250}
+                    fill="#8884d8"
+                    label={renderCustomizedLabel}
+                    labelLine={false}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={index} fill={`#${(Math.random() * 0xFFFFFF << 0).toString(16)}`} />
+
+                    ))}
+                    
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
+        </div >
+
+
+
+
     );
 };
 
