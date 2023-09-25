5d0bdd251d9c96221c9cb3f5cce6a66e982e2479
coder7475 <robiulhossain7475@gmail.com>
2023-09-25 21:52:54 +0600

    feat: add search button functionality

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,18 @@
+import PropTypes from 'prop-types';
 
-const Banner = () => {
+const Banner = ({  handleSearchClick }) => {
     return (
         <div className="h-[500px] flex flex-col justify-center items-center gap-[40px]">
             <h1 className="text-center text-[#0B0B0B] font-inter text-[48px] font-bold">I Grow By Helping People In Need</h1> 
-            <div className="flex">
-                <input type="text" name="search" id="search" className="w-80 h-[50px] rounded-l-lg p-4 text-[14px] font-normal font-inter text-[#0B0B0B66]" placeholder="Search Here"/>        
-                <button className="text-white w-[110px] h-[50px] bg-[#FF444A] rounded-r-lg text-[16px] font-semibold">Search</button>
-            </div>
+            <form className="flex" onSubmit={handleSearchClick}>
+                <input type="text" name="search" id="search" className="w-80 h-[50px] rounded-l-lg p-4 text-[14px] font-normal font-inter text-[#0B0B0B66]" placeholder="Search Here" />        
+                <input className="text-white w-[110px] h-[50px] bg-[#FF444A] rounded-r-lg text-[16px] font-semibold" type="submit"   value="Search"/> 
+            </form>
         </div>
     );
 };
-
+Banner.propTypes = {
+    // handleSearchBar: PropTypes.func,
+    handleSearchClick: PropTypes.func
+}
 export default Banner;
--- src/components/CardsDisplay/Cards.jsx
+++ src/components/CardsDisplay/Cards.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 
-const Cards = () => {
+const Cards = ({searchText}) => {
     const [cards, setCards] = useState([]);
     useEffect(() => {
         fetch('/Data/donation.json')
@@ -15,6 +15,12 @@ const Cards = () => {
         <div className="max-w-[1320px] mx-auto mt-24 mb-48 flex flex-col justify-center items-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {
+                ['food', 'health', 'education', 'clothing'].includes(searchText) ? 
+                cards.filter(card => {
+                    const category = card.category.toLowerCase();
+                    return category === searchText;
+                }).map(card => <Card card={card} key={card.id}></Card>)
+                :
                 cards.map(card => <Card card={card} key={card.id}></Card>)
 
             }           
