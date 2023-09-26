9be976345e4a5d853a54a8cc66093e35419e06ac
coder7475 <robiulhossain7475@gmail.com>
2023-09-26 21:08:33 +0600

    fix: image height for mobile in views detail page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -23,7 +23,7 @@ const Details = () => {
         <div className="max-w-[1320px] mx-auto mt-[80px]">
             <div className="relative">
                 <div className="absolute inset-x-0 bottom-0 h-[130px] bg-[#0B0B0B] bg-opacity-50"></div>
-                <img className="w-full rounded h-[700px]" src={picture} alt="donation picture" />
+                <img className="w-full rounded h-[300px] md:h-[700px]" src={picture} alt="donation picture" />
                 <div className="absolute bottom-9 left-9" onClick={handleDonateClick}>
                     <button onClick={notify} className=" text-white w-fit rounded h-[24px] px-6 py-4 flex justify-center items-center" style={{ backgroundColor: text_color }}>Donate $
                         <span className="text-[20px] font-semibold font-inter">
@@ -46,7 +46,7 @@ const Details = () => {
                 />
             </div>
             <h1 className="mt-[93px] text-[#0B0B0B] font-inter text-[40px] font-bold mb-6">{title}</h1>
-            <p className="text-[#0B0B0BB3] font-inter mb-[122px] font-normal text-[16px]">{description}</p>
+            <p className="text-[#0B0B0BB3] w-fit font-inter mb-[122px] font-normal text-[16px]">{description}</p>
         </div>
     );
 };
